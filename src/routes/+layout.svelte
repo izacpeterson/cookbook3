b87112bbf6d522c49ff872de2dd28824f512b7b9
izacpeterson <izacpeterson@gmail.com>
2023-11-20 09:29:04 -0700

    pwa

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,23 @@
 <script>
   import "../app.css";
   import { signIn, currentUser } from "$lib/firebase.js";
+  import { onMount } from "svelte";
   export let data;
+
+  onMount(() => {
+    if ("serviceWorker" in navigator) {
+      window.addEventListener("load", () => {
+        navigator.serviceWorker
+          .register("/service-worker.js")
+          .then((registration) => {
+            console.log("SW registered: ", registration);
+          })
+          .catch((registrationError) => {
+            console.log("SW registration failed: ", registrationError);
+          });
+      });
+    }
+  });
 </script>
 
 {#if data.uid}
